bf1f0251e31fadde02a10f6ea8d4e4f07849a7d1
psc1453 <pscpeng@outlook.com>
2021-06-15 07:37:44 +0800

    Add angle_preset.h, but wait to be filled in.

---
+++ Core/Inc/angle_preset.h
@@ -0,0 +1,22 @@
+//
+// Created by 彭世辰 on 2021/6/15.
+//
+
+#ifndef TDPS_PROJECT_PSC_ANGLE_PRESET_H
+#define TDPS_PROJECT_PSC_ANGLE_PRESET_H
+
+// TODO: Fill in the preset angle data
+// Patio 1 preset angles
+#define BRIDGE_TARGET_ANGLE 0
+#define GATE_TARGET_ANGLE 0
+
+// Patio 2 preset angles
+#define COLOR_RECOGNIZE_TARGET_ANGLE 0
+#define COLOR_LEFT_TARGET_ANGLE 0
+#define COLOR_CENTER_TARGET_ANGLE 0
+#define COLOR_RIGHT_TARGET_ANGLE 0
+#define FORWARD_TARGET_ANGLE 0
+#define LAKE_TARGET_ANGLE 0
+#define SIGNAL_TARGET_ANGLE 0
+
+#endif //TDPS_PROJECT_PSC_ANGLE_PRESET_H
